a752d2f29b0877dca150d3756be1f69e1c755223
purboyndra <purboindra@gmail.com>
2025-02-28 23:14:44 +0700

    feat: middleware/uplaod.ts

---
+++ src/middleware/upload.ts
@@ -0,0 +1,29 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const uploadDir = "uploads/";
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const fileFilter = (req: any, file: Express.Multer.File, cb: any) => {
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (ext === ".jpg" || ext === ".jpeg" || ext === ".png") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only .jpg, .jpeg, and .png files are allowed!"), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+export default upload;
