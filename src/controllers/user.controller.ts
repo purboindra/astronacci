c2e787453c634f32b8350df91c25e0632377dd93
purboyndra <purboindra@gmail.com>
2025-02-28 23:14:18 +0700

    feat: delete avatar

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import UserModel from "../models/user.model";
 import cloudinary from "../config/cloudinary";
-import multer from "multer";
+import { error } from "console";
 
 interface MulterRequest extends Request {
   file?: Express.Multer.File;
@@ -56,10 +56,24 @@ export const createUser = async (req: Request, res: Response) => {
 
 export const updateUser = async (req: Request, res: Response) => {
   try {
+    if (!req.params.id) {
+      res.status(400).json({ message: "User id is required" });
+      return;
+    }
+
+    if (!req.body) {
+      res.status(400).json({ message: "User data is required" });
+      return;
+    }
+
+    const avatar = await uploadAvatar(req, res);
+
+    const { address, age, name } = req.body;
+
     const updatedUser = await UserModel.findByIdAndUpdate(
       req.params.id,
       req.body,
-      { new: true },
+      { new: true }
     );
     if (!updatedUser) res.status(404).json({ message: "User not found" });
     res.json(updatedUser);
@@ -78,7 +92,45 @@ export const deleteUser = async (req: Request, res: Response) => {
   }
 };
 
-export const uploadAvatar = async (req: Request, res: Response) => {
+export const deleteAvatar = async (req: Request, res: Response) => {
+  try {
+    if (!req.params.id) {
+      res.status(400).json({ message: "User id is required" });
+      return;
+    }
+
+    const user = await UserModel.findById(req.params.id);
+
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    if (!user.avatar) {
+      res.status(404).json({ message: "Avatar not found" });
+      return;
+    }
+
+    const findPublicId = user.avatar.split("/").pop()?.split(".")[0];
+
+    cloudinary.uploader
+      .destroy(`avatars/${req.params.id}/${findPublicId}`)
+      .then(async () => {
+        user.avatar = null;
+        await user.save();
+        res.status(200).json({ message: "Avatar deleted successfully" });
+      })
+      .catch((error) => {
+        console.error("Error delete avatar", error);
+        res.status(500).json({ message: "Error delete avatar" });
+      });
+  } catch (e) {
+    console.error("Error delete avatar", error);
+    res.status(500).json({ message: "Error delete avatar" });
+  }
+};
+
+export const uploadAvatar = async (req: MulterRequest, res: Response) => {
   try {
     const userId = req.params.id;
 
@@ -94,28 +146,12 @@ export const uploadAvatar = async (req: Request, res: Response) => {
       return;
     }
 
-    const result = await cloudinary.uploader.upload(file.path, {
-      public_id: "avatars",
-    });
-
-    const optimizeUrl = cloudinary.url("avatars", {
-      fetch_format: "auto",
-      quality: "auto",
-    });
-
-    console.log(optimizeUrl);
-
-    const autoCropUrl = cloudinary.url("avatars", {
-      crop: "auto",
-      gravity: "auto",
-      width: 500,
-      height: 500,
-    });
+    const cloudinaryUrl = await uploadToCloudinary(file, userId);
 
     const user = await UserModel.findByIdAndUpdate(
       userId,
-      { avatar: result.secure_url },
-      { new: true },
+      { avatar: cloudinaryUrl },
+      { new: true }
     );
 
     res.status(200).json({
@@ -128,4 +164,33 @@ export const uploadAvatar = async (req: Request, res: Response) => {
       message: "Error upload avatar",
     });
   }
+};
+
+const uploadToCloudinary = async (
+  file: Express.Multer.File,
+  userId: string
+) => {
+  const uniquePublicId = `avatars/${userId}/${Date.now()}`;
+
+  const result = await cloudinary.uploader.upload(file.path, {
+    public_id: uniquePublicId,
+  });
+
+  console.log(result);
+
+  const optimizeUrl = cloudinary.url("avatars", {
+    fetch_format: "auto",
+    quality: "auto",
+  });
+
+  console.log(optimizeUrl);
+
+  const autoCropUrl = cloudinary.url("avatars", {
+    crop: "auto",
+    gravity: "auto",
+    width: 500,
+    height: 500,
+  });
+
+  return result.secure_url;
 };
