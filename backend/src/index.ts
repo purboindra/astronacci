bd2fe3242d96186547ae43434cfcc564223b4759
purboyndra <purboindra@gmail.com>
2025-03-01 13:51:55 +0700

    fix: change app.listen for handle consume via emulator

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,6 @@ import app from "./app";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+app.listen(parseInt(PORT.toString()), "0.0.0.0", () => {
+  console.log(`Server is running at http://0.0.0.0:3000`);
 });
